c932a61c2ed1d7678f984298a05dd5a9aca5ce7c
Swethamadda0710 <151740751+Swethamadda0710@users.noreply.github.com>
2026-01-27 20:12:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require ('express');
 const app = express();
  app.get("/",(req,res)=>{
-    res.send("welcome");
+    res.send("welcome Lakshmi Devi");
  });
 app.listen(3000, () => {
     console.log("Server running successfully");
